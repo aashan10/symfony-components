dfeb03aaf04b88cc6292919ab4546cb533160c9a
Ashan Ghimire <ashanghimire10@gmail.com>
2021-11-06 21:59:18 +0545

    create collection classes

---
+++ src/Collections/AbstractCollection.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Aashan\Workflow\Collections;
+
+use Aashan\Workflow\DataModel\DataModel;
+use Doctrine\Common\Collections\ArrayCollection;
+
+abstract class AbstractCollection extends ArrayCollection
+{
+    protected static string $primaryField = 'id';
+
+    public function find(string $id): DataModel|false
+    {
+        return $this->filter(function(object $dataObject) use($id) {
+            return (string) $dataObject->getData(static::$primaryField) === (string) $id;
+        })->first();
+    }
+}
---
+++ src/Collections/Collection.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Aashan\Workflow\Collections;
+
+class Collection extends AbstractCollection
+{
+    protected static string $primaryField = 'entity_id';
+
+}
